a5d470c677d9b85e02a7172f56568893c22033fd
DickM <dick@twocows.org>
2019-09-19 17:03:12 +0100

    byte stuff

--- ecopy.py
+++ ecopy.py
@@ -3,6 +3,7 @@ from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
 
 data = "Since we want to be able to encrypt an arbitrary amount of data, we use a hybrid encryption scheme. We use RSA with PKCS#1 OAEP for asymmetric encryption of an AES session key. The session key can then be used to encrypt all the actual data.".encode("utf-8")
+print(len(data))
 file_out = open("encrypted_data.bin", "wb")
 
 key = RSA.generate(2048)
@@ -18,4 +19,8 @@ enc_session_key = cipher_rsa.encrypt(session_key)
 # Encrypt the data with the AES session key
 cipher_aes = AES.new(session_key, AES.MODE_EAX)
 ciphertext, tag = cipher_aes.encrypt_and_digest(data)
-[ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
+# [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
+
+output = b''.join((enc_session_key, cipher_aes.nonce, tag, ciphertext))
+file_out.write(output)
+print(len(output))
